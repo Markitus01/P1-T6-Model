e867416604e2651fe252e9c12af4afe3f633dfbd
Markitus <marksand2001@gmail.com>
2024-12-06 22:25:00 +0100

    Update

---
+++ src/org/mif/manager/model/Categoria.java
@@ -0,0 +1,106 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.mif.manager.model;
+
+/**
+ *
+ * @author markos
+ */
+public class Categoria
+{
+    private int id;
+    private String nom;
+    private Integer edat_min;
+    private Integer edat_max;
+
+    public Categoria(){}
+
+    public int getId()
+    {
+        return id;
+    }
+    
+    // No cal comprovar correcte asignació d'id ja que es genera automàticament a la bbdd
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getNom()
+    {
+        return nom;
+    }
+
+    public void setNom(String nom)
+    {
+        if (nom == null || nom.length() == 0 || nom.length() > 16)
+        {
+            throw new RuntimeException("Nom de Categoria obligatori i amb 16 caràcters o menys");
+        }
+        
+        this.nom = nom;
+    }
+
+    public int getEdat_min()
+    {
+        return edat_min;
+    }
+
+    public void setEdat_min(Integer edat_min)
+    {
+        if (edat_min == null || edat_min == 0 || edat_min > 99)
+        {
+            throw new RuntimeException("Edat mínima no pot ser 0 ni tampoc més gran de 99");
+        }
+        
+        this.edat_min = edat_min;
+    }
+
+    public int getEdat_max()
+    {
+        return edat_max;
+    }
+
+    public void setEdat_max(Integer edat_max)
+    {
+        if (edat_max == 0 || edat_max > 99)
+        {
+            throw new RuntimeException("Edat màxima no pot ser 0 ni tampoc més gran de 99");
+        }
+        
+        this.edat_max = edat_max;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Categoria other = (Categoria) obj;
+        return this.id == other.id;
+    }
+
+    
+
+    @Override
+    public String toString() {
+        return "Categoria{" + "Nom=" + nom + ", Edat mínima=" + edat_min + ", Edat màxima=" + edat_max + '}';
+    }
+    
+    
+}
---
+++ src/org/mif/manager/model/Equip.java
@@ -0,0 +1,123 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.mif.manager.model;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author markos
+ */
+public class Equip
+{
+    private int id;
+    private String nom;
+    private String tipus;
+    private Categoria categoria;
+    private LocalDate temporada;
+
+    public Equip(){}
+    
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getNom()
+    {
+        return nom;
+    }
+
+    public void setNom(String nom)
+    {
+        if (nom == null || nom.length() == 0 || nom.length() > 32)
+        {
+            throw new RuntimeException("Nom obligatori i no més llarg de 32 caràcters");
+        }
+        
+        this.nom = nom;
+    }
+
+    public String getTipus()
+    {
+        return tipus;
+    }
+
+    public void setTipus(String tipus)
+    {
+        if (tipus == null || tipus.length() != 1)
+        {
+            throw new RuntimeException("Tipus és obligatori.");
+        }
+        
+        this.tipus = tipus;
+    }
+
+    public Categoria getCategoria()
+    {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria)
+    {
+        if (categoria == null)
+        {
+            throw new RuntimeException("Categoria és obligatoria");
+        }
+        
+        this.categoria = categoria;
+    }
+
+    public LocalDate getTemporada()
+    {
+        return temporada;
+    }
+
+    public void setTemporada(LocalDate temporada)
+    {
+        if (temporada == null)
+        {
+            throw new RuntimeException("Temporada és obligatoria.");
+        }
+        
+        this.temporada = temporada;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 37 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Equip other = (Equip) obj;
+        return this.id == other.id;
+    }
+
+    
+
+    @Override
+    public String toString() {
+        return "Equip{Id="+ id +", Nom=" + nom + ", Tipus=" + tipus + ", Categoria=" + categoria.getNom() + ", Temporada=" + temporada + '}';
+    }
+    
+    
+}
